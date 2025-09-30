ff61dca0a3d02dbb91dae198281549a292929fe1
SoUrAbHvrMa <41387688+SoUrAbHvrMa@users.noreply.github.com>
2025-09-30 23:31:04 +0530

    Message Service
    pushing message service

---
+++ Services/message-service/src/main/java/com/message/user_message/MessageApplication.java
@@ -0,0 +1,13 @@
+package com.message.user_message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageApplication.class, args);
+	}
+
+}
